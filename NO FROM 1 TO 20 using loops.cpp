17dbef97d77646f76fc4962f4db4746d2ec4ac28
Ashutosh Singh <ashutosh220kharayat@gmail.com>
2024-08-11 19:48:51 +0530

    Add files via upload

---
+++ NO FROM 1 TO 20 using loops.cpp
@@ -0,0 +1,38 @@
+// print numbers from 1 to 20 using loops
+// ASHUTOSH SINGH
+// 23070123033
+ 
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    n=1;
+    while(n<=20){
+        cout<<n<<endl;
+        n++;
+    }
+   
+} 
+
+// Output
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20*/
